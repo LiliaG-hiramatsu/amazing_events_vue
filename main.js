a1468bd98df93f8d20f53d10b159c88f07aadf70
Lilia <liliagarciahiramatsu@gmail.com>
2023-03-21 13:31:09 -0300

    pagina home terminada

--- main.js
+++ main.js
@@ -3,20 +3,58 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            mensaje: "Hola"
+            urlApi: 'https://mindhub-xj03.onrender.com/api/amazing',
+            eventos: [],
+            backupEventos: [],
+            texto: '',
+            categorias: [],
+            categoriasChecked: []
         }
     },
     created() {
-
+        this.traerDatos()
     },
     mounted() {
 
     },
     methods: {
-
+        traerDatos() {
+            fetch(this.urlApi)
+                .then(response => response.json())
+                .then(data => {
+                    this.eventos = data.events
+                    this.backupEventos = this.eventos
+                    this.extraerCategorias(this.eventos)
+                })
+                .catch(error => console.log(error.message))
+        },
+        extraerCategorias(arr) {
+            arr.forEach(elemento => {
+                if (!this.categorias.includes(elemento.category)) {
+                    this.categorias.push(elemento.category)
+                }
+            })
+        }
     },
     computed: {
-
+        filtrarPorBuscador() {
+            this.eventos = this.backupEventos.filter(evento => evento.name.toLowerCase().includes(this.texto.toLowerCase()))
+        },
+        filtrarPorCategoria() {
+            if (this.categoriasChecked.length > 0) {
+                this.eventos = this.backupEventos.filter(evento => this.categoriasChecked.includes(evento.category))
+            } else {
+                this.eventos = this.backupEventos
+            }
+        },
+        filtroDoble() {
+            let primerFiltro = this.backupEventos.filter(evento => evento.name.toLowerCase().includes(this.texto.toLowerCase()))
+            if (this.categoriasChecked.length > 0) {
+                this.eventos = primerFiltro.filter(evento => this.categoriasChecked.includes(evento.category))
+            } else {
+                this.eventos = primerFiltro
+            }
+        }
     }
 })
 
